d929657f83bf5c464a5bd290a0b1fabda2678619
Aasemaaneh <aasemaaneh8@email.com>
2023-05-23 20:27:53 -0400

    take in an array and return the middle-most element(s) of the given array.

---
+++ middle.js
@@ -0,0 +1,34 @@
+
+const assertArraysEqual = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+        return;
+      }
+    }
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+  }
+};
+
+
+const middle = function(array) {
+  const middleIndex = Math.floor(array.length / 2);
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 1) {
+    return [array[middleIndex]];
+  } else {
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+};
+
+// TEST CODE
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
