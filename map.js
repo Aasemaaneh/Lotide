a1e73202a257421f38f26ebf5f045ba159502fbb
Aasemaaneh <aasemaaneh8@gmail.com>
2023-06-02 22:55:01 -0400

    the faunction that gets array and callback function and return the result.

--- map.js
+++ map.js
@@ -1,12 +1,4 @@
-const assertArraysEqual = function(array1, array2) {
-    if (eqArrays(array1,array2)) {
-      console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
-        }
-    }
-
-  const eqArrays = function(array1, array2) {
+const eqArrays = function(array1, array2) {
     if (array1.length !== array2.length) {
       return false;
     }
@@ -19,25 +11,30 @@ const assertArraysEqual = function(array1, array2) {
     
     return true;
   };
-    
-  
-  const map = function(array, callback) {
-    const results = [];
-    for (let item of array) {
-      results.push(callback(item));
-    }
-    return results;
-  };
-  
-  // Test cases
-  const words = ["ground", "control", "to", "major", "tom"];
-  
-  const results1 = map(words, word => word[0]);
-  assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
-  const results2 = map(words, word => Math.round(Math.sqrt(word.length)));
-  assertArraysEqual(results2, [ 2, 3, 1, 2, 2 ]);
-  const results3 = map(words, word => word.toUpperCase() + " : " + word);
-  assertArraysEqual(results3, ['GROUND : ground','CONTROL : control','TO : to','MAJOR : major','TOM : tom'
-  ]);
+const assertArraysEqual = function(array1, array2) {
+    if (eqArrays(array1,array2)) {
+      console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+        }
+};
  
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
 
+// Test cases
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+const results2 = map(words, word => Math.round(Math.sqrt(word.length)));
+assertArraysEqual(results2, [ 2, 3, 1, 2, 2 ]);
+const results3 = map(words, word => word.toUpperCase() + " : " + word);
+assertArraysEqual(results3, ['GROUND : ground','CONTROL : control','TO : to','MAJOR : major','TOM : tom'
+]);
+ 
